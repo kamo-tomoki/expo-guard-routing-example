b9537f0e1f354292c613e8d4efa4281a13d0c438
Kamo Tomoki <tomokiti919@gmail.com>
2024-07-25 23:04:25 +0900

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,8 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
-      <Stack.Screen name="user/[id]/index" />
+      <Stack.Screen name="another-page" />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,41 @@
 import { router } from "expo-router";
-import { Button, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [inputText, setInputText] = useState("");
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
     },
+    text: {
+      fontSize: 25,
+    },
+    textInput: {
+      marginTop: 100,
+      padding: 15,
+      borderRadius: 15,
+      borderWidth: 1,
+      width: 300,
+    },
   });
 
   return (
     <View style={styles.container}>
-      <Button
-        title="User 3"
-        onPress={() => router.navigate("/user/3?oauth=true")}
+      <Text style={styles.text}>{inputText}</Text>
+      <TextInput
+        style={styles.textInput}
+        value={inputText}
+        onChangeText={setInputText}
       />
       <Button
-        title="User 12"
-        onPress={() => router.navigate("/user/12?oauth=false")}
+        title="Another Page"
+        onPress={() => router.navigate("/another-page")}
       />
+      <Button title="Modal" onPress={() => router.navigate("/modal")} />
     </View>
   );
 }
